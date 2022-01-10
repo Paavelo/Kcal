b78dcd452a76eaaa9a62d2aa850ae61681d17e39
Paavelo <97466071+Paavelo@users.noreply.github.com>
2022-01-10 23:20:01 +0600

    Add files via upload

---
+++ kcal.sh
@@ -0,0 +1,3 @@
+#!/system/bin/sh # Kcal by Pavel 
+KXLOG=/data/media/0/KTSR/kxvision.log [[ -e ${KXLOG} ]] && rm -rf "${KXLOG}" # King Xvision 
+echo "1" >"/sys/devices/platform/kcal_ctrl.0/kcal_enable" echo "229 229 229" >"/sys/devices/platform/kcal_ctrl.0/kcal" echo "265" >"/sys/devices/platform/kcal_ctrl.0/kcal_sat" echo "255" >"/sys/devices/platform/kcal_ctrl.0/kcal_val" echo "255" >"/sys/devices/platform/kcal_ctrl.0/kcal_cont" [[ $? == "0" ]] && { 	echo "[*] KCAL Xvision preset executed without any errors!" >"${KXLOG}" 	exit 0 } || { 	service call SurfaceFlinger 1023 i32 0 >/dev/null 2>&1 	service call SurfaceFlinger 1022 f 1.5 >/dev/null 2>&1 	echo "[!] KCAL preset executed with errors, using SF native saturation boost function instead." >"${KXLOG}" 	exit $? }
